b72d14ff853d317bf38a5c7637620b441fb055ab
张忠 <>
2018-06-15 17:14:22 +0800

    Save before JSON

---
+++ Signal/src/network/Wallet/BBRequestHelper.swift
@@ -0,0 +1,15 @@
+//
+//  BBRequestHelper.swift
+//  Signal
+//
+//  Created by 张忠 on 2018/6/15.
+//  Copyright © 2018年 Open Whisper Systems. All rights reserved.
+//
+
+import UIKit
+
+class BBRequestHelper: NSObject {
+    static func parseSuccessResult(object:Any) -> JSON{
+        
+    }
+}
